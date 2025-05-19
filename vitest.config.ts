f1d8b129cc54a7e3de17dffa4ed58fc4b4b3df46
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-05-19 17:11:14 -0400

    added vitest config to exclude stuff

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vitest/config";
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            vue: "vue/dist/vue.esm-bundler.js",
+        },
+    },
+    test: {
+        coverage: {
+            exclude: [
+                "**/*.vue", // Components
+                "*.ts", // Root of repo (mainly config files)
+                "src/*.ts", // Root of repo (mainly config files)
+                "**/*.config.ts", // All other config files
+                "dist/**", // Build
+                "src/i18n/**", // Nothing to test here
+            ],
+        },
+    },
+});
