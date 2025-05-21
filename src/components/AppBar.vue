a80a97ff84e1577219e6c6cc0786d9c1b67ae0e4
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-05-20 23:12:16 -0400

    continued app bar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,8 +1,26 @@
 <template>
     <header class="shadow">
         <div class="appbar-container">
-            <nav class="appbar-navigation">
-                <router-link class="appbar-item appbar-title" :to="{ name: ROUTES.home.name }"> App Name </router-link>
+            <nav class="appbar-items">
+                <router-link class="appbar-item appbar-item-title" :to="{ name: ROUTES.home.name }">
+                    App Name
+                </router-link>
+
+                <router-link
+                    v-if="!isAuthenticated"
+                    :to="{ name: ROUTES['not-found'].name }"
+                    class="appbar-item appbar-item-text"
+                >
+                    Dashboard
+                </router-link>
+
+                <router-link
+                    v-if="!isAuthenticated"
+                    :to="{ name: ROUTES['not-found'].name }"
+                    class="appbar-item appbar-item-text"
+                >
+                    Dashboard
+                </router-link>
 
                 <router-link
                     v-if="!isAuthenticated"
@@ -13,15 +31,17 @@
                 </router-link>
             </nav>
 
-            <nav class="appbar-util">
-                <router-link class="appbar-item appbar-title" :to="{ name: ROUTES.home.name }"> App Name </router-link>
+            <nav class="appbar-utils">
+                <router-link :to="{ name: ROUTES.home.name }" class="appbar-util">
+                    <HomeIcon class="appbar-util-icon" />
+                </router-link>
 
-                <router-link
-                    v-if="!isAuthenticated"
-                    :to="{ name: ROUTES['not-found'].name }"
-                    class="appbar-item appbar-item-text"
-                >
-                    Dashboard
+                <router-link :to="{ name: ROUTES.home.name }" class="appbar-util">
+                    <HomeIcon class="appbar-util-icon" />
+                </router-link>
+
+                <router-link :to="{ name: ROUTES.home.name }" class="appbar-util">
+                    <HomeIcon class="appbar-util-icon" />
                 </router-link>
             </nav>
         </div>
@@ -32,6 +52,7 @@
 import { useUserSessionStore } from "@stores/user/userSessionStore";
 import { computed } from "vue";
 import { ROUTES } from "@router/routes";
+import { HomeIcon } from "@heroicons/vue/24/outline";
 
 const userSessionStore = useUserSessionStore();
 const isAuthenticated = computed(() => userSessionStore.isAuthenticated());
