b31fd8d7e933010f618c3292ff76546b96d3cf20
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-05-20 21:18:47 -0400

    feat: using tailwind for css styling

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,4 +1,6 @@
 <template>
+    <h1 class="text-3xl font-bold underline funky-title">Hello world!</h1>
+
     <HelloWorld msg="Vite + Vue" />
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, type PluginOption } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { visualizer } from "rollup-plugin-visualizer";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
             template: "flamegraph",
             open: false,
         }) as PluginOption,
+        tailwindcss(),
     ],
     resolve: {
         alias: {
