4489fdd11d66dacfb51edcf5b226d645cb10f998
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-05-21 19:15:55 -0400

    added secondary items and support for either links or icons

--- src/components/appbar/AppBar.vue
+++ src/components/appbar/AppBar.vue
@@ -16,24 +16,11 @@
             </nav>
 
             <nav class="appbar-secondary-items">
-                <router-link
-                    :to="{ name: ROUTES.not_found.name }"
-                    class="appbar-secondary-item appbar-secondary-item-clickable"
-                >
-                    <UserIcon class="appbar-secondary-item-icon" />
-                </router-link>
+                <SecondaryItemLink :icon="UserIcon" :route-name="ROUTES.not_found.name" />
 
-                <button
-                    @click="userSessionStore.changeTheme"
-                    class="appbar-secondary-item appbar-secondary-item-clickable"
-                >
-                    <SunIcon v-if="isDarkTheme" class="appbar-secondary-item-icon" />
-                    <MoonIcon v-else class="appbar-secondary-item-icon" />
-                </button>
+                <SecondaryItemIcon @click="userSessionStore.changeTheme" :icon="themeIcon" />
 
-                <button @click="" class="appbar-secondary-item appbar-secondary-item-clickable">
-                    <Cog8ToothIcon class="appbar-secondary-item-icon" />
-                </button>
+                <SecondaryItemIcon @click="" :icon="Cog8ToothIcon" />
             </nav>
         </div>
     </header>
@@ -43,15 +30,18 @@
 import { useUserSessionStore } from "@stores/user/userSessionStore";
 import { computed } from "vue";
 import { ROUTES } from "@router/routes";
-import { HomeIcon } from "@heroicons/vue/24/outline";
 import { Cog8ToothIcon } from "@heroicons/vue/24/outline";
 import { MoonIcon } from "@heroicons/vue/24/outline";
 import { SunIcon } from "@heroicons/vue/24/outline";
 import { UserIcon } from "@heroicons/vue/24/outline";
 import Link from "@components/appbar/Link.vue";
 import SiteTitle from "@components/appbar/SiteTitle.vue";
+import SecondaryItemIcon from "@components/appbar/SecondaryItemIcon.vue";
+import SecondaryItemLink from "@components/appbar/SecondaryItemLink.vue";
 
 const userSessionStore = useUserSessionStore();
+
 const isAuthenticated = computed(() => userSessionStore.isAuthenticated());
 const isDarkTheme = computed(() => userSessionStore.isDarkTheme());
+const themeIcon = computed(() => (isDarkTheme.value ? SunIcon : MoonIcon));
 </script>
---
+++ src/components/appbar/SecondaryItemLink.vue
@@ -0,0 +1,31 @@
+<template>
+    <button v-bind="$attrs">
+        <router-link v-if="icon" :to="{ name: props.routeName }">
+            <SecondaryItemIcon :icon="icon" />
+        </router-link>
+
+        <Link v-else-if="displayedText" :displayed-text="displayedText" :route-name="routeName" />
+
+        <slot v-else></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+import Link from "@components/appbar/Link.vue";
+import SecondaryItemIcon from "@components/appbar/SecondaryItemIcon.vue";
+
+const props = defineProps({
+    icon: {
+        required: false,
+        type: Function,
+    },
+    routeName: {
+        required: true,
+        type: String,
+    },
+    displayedText: {
+        required: false,
+        type: String,
+    },
+});
+</script>
