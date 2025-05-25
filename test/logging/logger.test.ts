65b5ab5b4f0cdd11e0d180683479de07f23efccc
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-05-25 15:18:05 -0400

    logger testing

---
+++ test/logging/logger.test.ts
@@ -0,0 +1,34 @@
+import { expect, test, describe, beforeEach, vi } from "vitest";
+import { POP_LOG_DEBUG } from "@/logging/logger";
+import type { EnvMode } from "@/logging/types";
+
+describe("logging/logger", () => {
+    const ANY_DATA = {
+        hello: "world",
+    };
+
+    beforeEach(() => {
+        vi.restoreAllMocks();
+        vi.unstubAllEnvs();
+    });
+
+    test("whenUsingLoggingInProduction_thenShouldNotLogToConsole", () => {
+        const envMode: EnvMode = "production";
+        vi.stubEnv("MODE", envMode);
+
+        const consoleSpy = vi.spyOn(console, "log");
+        POP_LOG_DEBUG(ANY_DATA);
+
+        expect(consoleSpy).not.toHaveBeenCalled();
+    });
+
+    test("whenUsingLoggingInDevelopment_thenShouldLogToConsole", () => {
+        const envMode: EnvMode = "development";
+        vi.stubEnv("MODE", envMode);
+
+        const consoleSpy = vi.spyOn(console, "log");
+        POP_LOG_DEBUG(ANY_DATA);
+
+        expect(consoleSpy).toHaveBeenCalledWith(ANY_DATA);
+    });
+});
