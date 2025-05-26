2f764c8edd2a38f44a0674f569bc961e45527681
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-05-26 19:39:41 -0400

    correctly changing theme in theme instance

--- src/services/themes/themeInstance.ts
+++ src/services/themes/themeInstance.ts
@@ -1,11 +1,13 @@
 import type { SupportedTheme } from "@/services/themes/types";
 import { SUPPORTED_THEMES } from "@/services/themes/constants";
+import { useColorMode } from "@vueuse/core";
 
 export class ThemeInstance {
     private theme: SupportedTheme<any> = SUPPORTED_THEMES.light;
 
     public setTheme(theme: SupportedTheme<any>): void {
         this.theme = theme;
+        useColorMode().value = theme.label;
     }
 
     public getTheme(): SupportedTheme<any> {
