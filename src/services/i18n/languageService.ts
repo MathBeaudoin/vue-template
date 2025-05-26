c0fb9f96f73d9b58cd0bc79013edd4658579ae75
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-05-26 18:40:01 -0400

    added logging to services

--- src/services/i18n/languageService.ts
+++ src/services/i18n/languageService.ts
@@ -1,6 +1,7 @@
 import { I18nInstance } from "@/services/i18n/i18nInstance";
 import type { SupportedLanguage, SupportedLocale } from "@/services/i18n/types";
 import { SUPPORTED_LANGUAGES } from "@/services/i18n/constants";
+import { POP_LOG_DEBUG } from "@/logging/logger";
 
 export class LanguageService {
     private static i18nInstance = new I18nInstance();
@@ -14,6 +15,7 @@ export class LanguageService {
     }
 
     public static selectLanguage(language: SupportedLanguage<any>): Error | SupportedLocale {
+        POP_LOG_DEBUG(`LanguageService - selectLanguage (${language.locale})`);
         if (!this.isValidLanguage(language)) {
             return new Error();
         }
--- src/services/themes/themeService.ts
+++ src/services/themes/themeService.ts
@@ -1,6 +1,7 @@
 import type { SupportedTheme, SupportedThemeLabel } from "@/services/themes/types";
 import { SUPPORTED_THEMES } from "@/services/themes/constants";
 import { ThemeInstance } from "@/services/themes/themeInstance";
+import { POP_LOG_DEBUG } from "@/logging/logger";
 
 export class ThemeService {
     private static themeInstance = new ThemeInstance();
@@ -10,6 +11,7 @@ export class ThemeService {
     }
 
     public static selectTheme(theme: SupportedTheme<any>): Error | SupportedThemeLabel {
+        POP_LOG_DEBUG(`ThemeService - selectTheme (${theme.label})`);
         if (!this.isValidTheme(theme)) {
             return new Error();
         }
